779404bc6569d9f53714a0358d8991d09585a8d3
PHYO-AUNG-NAING-TUN <phyotun24@gmail.com>
2025-03-15 10:06:42 +0630

    added font and update register.php

--- resource/view/auth/register.php
+++ resource/view/auth/register.php
@@ -6,6 +6,13 @@
             <input class=" w-full border border-gray-300 h-[54px] ps-3 rounded-xl" type="name" placeholder="Name">
             <input class=" w-full border border-gray-300 h-[54px] ps-3 rounded-xl" type="email" placeholder="Email">
             <input class=" w-full border border-gray-300 h-[54px] ps-3 rounded-xl" type="password" placeholder="Password">
+            <div class=" flex items-center space-x-3">
+                <input type="checkbox" class=" accent-purple-600 flex-1 min-w-5 min-h-5">
+                <div class="">
+                    <span class=" font-semibold">By signing up, you agree to the</span>
+                    <a class=" text-primary" href="#">Terms of Service and Privacy Policy</a>
+                </div>
+            </div>
             <div class=" space-y-3">
                 <button class=" w-full h-[54px] tracking-wide font-bold bg-primary rounded-xl text-white text-lg">Sign Up</button>
                 <div class=" text-gray-400 text-center font-bold tracking-wide">Or with</div>
@@ -17,7 +24,7 @@
         </form>
         <div class="tracking-wide flex justify-center">
             <span class=" me-2 ">Already have an account? </span>
-            <a class="text-primary font-semibold" href="/register">Login </a>
+            <a class="text-primary font-semibold" href="/login">Login </a>
         </div>
     </div>
 </div>
