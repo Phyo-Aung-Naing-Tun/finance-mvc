79aaae010e66901faa7d4f186a472fae890e5b19
PHYO-AUNG-NAING-TUN <phyotun24@gmail.com>
2025-03-15 10:31:37 +0630

    update headerNavigation

--- resource/view/components/headerNavigation.php
+++ resource/view/components/headerNavigation.php
@@ -1,6 +1,6 @@
 <div>
     <div class="grid grid-cols-4 p-3 items-center ">
-        <a href="<?= $route ? $route : "/" ?>" class=" text-start col-span-1"><img src="/storage/images/arrowLeft.svg" alt=""></a>
+        <a href="<?= $route ?? "/" ?>" class=" text-start col-span-1"><img src="/storage/images/arrowLeft.svg" alt=""></a>
         <div class="col-span-2 text-center font-semibold text-xl  capitalize"><?= $title ?? null ?></div>
         <?php if ($showMenuButton ?? false): ?>
             <p class="col-span-1 text-end">Menu button</p>
