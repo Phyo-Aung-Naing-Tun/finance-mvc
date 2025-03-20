b692716df8defb4f277d5495ffaf2fa18a0f982e
PHYO-AUNG-NAING-TUN <phyotun24@gmail.com>
2025-03-20 21:31:47 +0630

    added database config file

--- app/configs/database.php
+++ app/configs/database.php
@@ -2,12 +2,20 @@
 
 return [
     "mysql" => [
-        "host" => "localhost",
-        "dbname" => "mydb",
-        "username" => "root",
-        "password" => "",
-        "port" => [
-            "name" => 34343
-        ]
-    ]
+        "connection" => "mysql",
+        "host" =>  env("DATABASE_HOST") ?? "localhost",
+        "dbname" => env("DATABASE_NAME") ?? "dbname",
+        "username" => env("DATABASE_USERNAME") ?? "root",
+        "password" => env("DATABASE_PASSWORD") ?? "",
+        "port" => env("DATABASE_PORT") ?? 101
+    ],
+    "pgsql" => [
+        "connection" => "pgsql",
+        "host" =>  env("DATABASE_HOST") ?? "localhost",
+        "dbname" => env("DATABASE_NAME") ?? "dbname",
+        "username" => env("DATABASE_USERNAME") ?? "root",
+        "password" => env("DATABASE_PASSWORD") ?? "",
+        "port" => env("DATABASE_PORT") ?? 101
+    ],
+    // you can extends more
 ];
