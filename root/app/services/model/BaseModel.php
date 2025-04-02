4b4a318eff368ea25add37d9687048790c83e526
PHYO-AUNG-NAING-TUN <phyotun24@gmail.com>
2025-04-02 21:27:43 +0630

    added auto generate table name system

--- root/app/services/model/BaseModel.php
+++ root/app/services/model/BaseModel.php
@@ -25,8 +25,36 @@ class BaseModel extends MainService
     {
         if (!($this->database && $this->connection)) {
             $this->database =  (new DBFactory($this->connection))->build();
+            $this->table = $this->table ? $this->table : $this->createTableName();
             $this->queryBuilder = new QueryBuilder($this->database, $this->table);
         }
+    }
+
+    protected  function createTableName()
+    {
+        $vowels = ["a", "e", "i", "o", "u"];
+        $callClass = get_called_class();
+        $callClass = explode('\\', $callClass);
+        $callClass = end($callClass);
+        $callClass = strtolower($callClass);
+        $callClassArray = str_split($callClass);
+        $lastTwoWords =  array_slice($callClassArray, -2);
+        if (end($lastTwoWords) == "s" || end($lastTwoWords) == "x" || end($lastTwoWords) == "z") {
+            return $callClass . "es";
+        } elseif (end($lastTwoWords) == "y") {
+            $firstWord = array_shift($lastTwoWords);
+            $firstWordVowel = in_array($firstWord, $vowels);
+            if (!$firstWordVowel) {
+                array_pop($callClassArray); //cut the y  and add ies
+                return join($callClassArray) . "ies";
+            }
+        } elseif (end($lastTwoWords) == "h") {
+            $firstWord = array_shift($lastTwoWords);
+            if ($firstWord == "c" || $firstWord == "s") {
+                return $callClass . "es";
+            }
+        }
+        return $callClass . "s";
     }
 
     protected function validateMethod($method)
